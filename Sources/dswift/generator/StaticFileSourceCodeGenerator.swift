b85859546cd7f29265e833599d9ce347189ceaec
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2019-09-21 17:29:46 -0400

    Added missing logic for new defined methods in the DynamicGenerator protocol to move logic out of the package generate-xcodeproj to the generator Fixed issue with the \ character when loading text files.  It will now escape all \ with \\

--- Sources/dswift/generator/StaticFileSourceCodeGenerator.swift
+++ Sources/dswift/generator/StaticFileSourceCodeGenerator.swift
@@ -154,6 +154,124 @@ public class StaticFileSourceCodeGenerator: DynamicGenerator {
         return XcodeFileType.Text.json
     }
     
+    public func generateSourceCodeRequired(for source: String) throws -> Bool {
+        let source = URL(fileURLWithPath: source)
+        let staticFile: StaticFile = try JSONDecoder().decode(StaticFile.self,
+                                                           from: try Data(contentsOf: source))
+        let staticFilePath = staticFile.file.fullPath(from: source.deletingLastPathComponent().path)
+        let staticFileURL = URL(fileURLWithPath: staticFilePath)
+        
+        let destination = try generatedFilePath(for: source)
+        
+        // If the generated file does not exist, then return true
+        guard FileManager.default.fileExists(atPath: destination.path) else {
+            verbosePrint("Generated file '\(destination.path)' does not exists")
+            return true
+        }
+        // Get the modification dates otherwise return true to rebuild
+        guard let srcMod = source.pathModificationDate,
+              let staticSrcMod = staticFileURL.pathModificationDate,
+              let desMod = destination.pathModificationDate else {
+                verbosePrint("Wasn't able to get all modification dates")
+            return true
+        }
+        // Source or static file is newer than destination, meaning we must rebuild
+        guard srcMod <= desMod && staticSrcMod <= desMod else {
+            if srcMod > desMod {
+                verbosePrint("'\(source.path)' is newer")
+            } else  if staticSrcMod > desMod {
+                verbosePrint("'\(staticFileURL.path)' is newer")
+            }
+            return true
+        }
+        
+        var encoding: String.Encoding = .utf8
+        let fileContents = try String(contentsOf: destination, usedEncoding: &encoding)
+        if fileContents.contains("// Failed to generate source code.") { return true }
+        return false
+    }
+    
+    public func addToXcodeProject(xcodeFile: XcodeFileSystemURLResource,
+                                  inGroup group: XcodeGroup,
+                                  havingTarget target: XcodeTarget) throws -> Bool {
+        var rtn: Bool = true
+        
+        let staticFilePath: String? = {
+            do {
+                let source = URL(fileURLWithPath: xcodeFile.path)
+                let staticFile: StaticFile = try JSONDecoder().decode(StaticFile.self,
+                                                                      from: try Data(contentsOf: source))
+                return staticFile.file.fullPath(from: xcodeFile.path.deletingLastPathComponent)
+            } catch {
+                return nil
+            }
+        }()
+        
+        if group.file(atPath: xcodeFile.lastPathComponent) == nil {
+            // Only add the dswift file to the project if its not already there
+            
+            let f = try group.addExisting(xcodeFile,
+                                          copyLocally: true,
+                                          savePBXFile: false) as! XcodeFile
+            
+            //f.languageSpecificationIdentifier = "xcode.lang.swift"
+            f.languageSpecificationIdentifier = self.languageForXcode(file: xcodeFile.path)
+            f.explicitFileType = self.explicitFileTypeForXcode(file: xcodeFile.path)
+            target.sourcesBuildPhase().createBuildFile(for: f)
+            //print("Adding dswift file '\(child.path)'")
+        }
+        if let sFile = staticFilePath {
+            verbosePrint("Found static file '\(sFile)' for adding to project")
+            
+            if !FileManager.default.fileExists(atPath: sFile) {
+                errPrint("WARNING: Static file '\(sFile)' does not exist.")
+            }
+    
+            let rootGroupPath = group.mainGroup.fullPath
+            if let r = sFile.range(of: rootGroupPath), r.lowerBound == sFile.startIndex {
+                var staticFileRelativePath = String(sFile[r.upperBound...])
+                if !staticFileRelativePath.hasPrefix("/") { staticFileRelativePath = "/" + staticFileRelativePath }
+                let staticFileParentPath = staticFileRelativePath.deletingLastPathComponent
+                let staticFileParentGroup: XcodeGroup = try group.mainGroup.subGroup(atPath: staticFileParentPath,
+                                                                                     options: .createOnly)!
+                if !staticFileParentGroup.contains(where: { $0.name == sFile.lastPathComponent }) {
+                verbosePrint("Adding '\(staticFileRelativePath)' to project")
+                try staticFileParentGroup.addExisting(XcodeFileSystemURLResource(file: sFile),
+                                                      copyLocally: true,
+                                                      savePBXFile: false)
+                } else {
+                    errPrint("A file with the same name '\(sFile.lastPathComponent)' already exists in '\(staticFileParentGroup.relativePath)'")
+                    rtn = false
+                }
+            } else {
+                errPrint("ERROR: Static file '\(sFile)' is not within the project")
+                rtn = false
+            }
+            
+        } else {
+            errPrint("ERROR: Could not load path for static file in '\(xcodeFile.path)'")
+            rtn = false
+        }
+        let swiftName = try self.generatedFilePath(for: xcodeFile.path).lastPathComponent
+        if let f = group.file(atPath: swiftName) {
+            var canRemoveSource: Bool = true
+            do {
+                let source = try String(contentsOf: URL(fileURLWithPath: f.fullPath))
+                if !source.hasPrefix("//  This file was dynamically generated from") {
+                    rtn = false
+                    errPrint("Error: Source file '\(f.fullPath)' matches build file name for '\(xcodeFile.path)' and is NOT a generated file")
+                    canRemoveSource = false
+                }
+                
+            } catch { }
+            if canRemoveSource {
+                // Remove the generated swift file if its there
+                try f.remove(deletingFiles: false, savePBXFile: false)
+            }
+        }
+        return rtn
+    }
+    
     public func generateSource(from sourcePath: String,
                                havingEncoding encoding: String.Encoding? = nil) throws -> (source: String, encoding: String.Encoding) {
         
@@ -187,7 +305,8 @@ public class StaticFileSourceCodeGenerator: DynamicGenerator {
         
         if srcFile.type.isText {
             
-            let stringValue = try String(contentsOf: workingFileURL, usedEncoding: &encoding)
+            var stringValue = try String(contentsOf: workingFileURL, usedEncoding: &encoding)
+            stringValue = stringValue.replacingOccurrences(of: "\\", with: "\\\\")
             sourceCode += structTabs + "\t\(srcFile.modifier) static let string: String = \"\"\"\n"
             sourceCode += "\(stringValue)"
             sourceCode += "\n"
