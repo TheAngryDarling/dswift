78ffc4a1ffea651473679caced0548d32aa4fac3
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2019-09-21 17:20:06 -0400

    Fixed issue where if NO_DSWIFT_PARAMS was set it wouldn't load the swift path from the config file and only use the default path.  I had to reverse the logic

--- Sources/dswift/objects/DSwiftSettings.swift
+++ Sources/dswift/objects/DSwiftSettings.swift
@@ -186,9 +186,10 @@ extension DSwiftSettings: Codable {
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         #if NO_DSWIFT_PARAMS
-        self.swiftPath = try container.decodeIfPresent(String.self, forKey: .swiftPath) ?? DSwiftSettings.defaultSwiftPath
-        #else
         self.swiftPath = DSwiftSettings.defaultSwiftPath
+        
+        #else
+        self.swiftPath = try container.decodeIfPresent(String.self, forKey: .swiftPath) ?? DSwiftSettings.defaultSwiftPath
         #endif
         self.xcodeResourceSorting = try container.decodeIfPresent(FileResourceSorting.self, forKey: .xcodeResourceSorting) ?? FileResourceSorting.none
         self.license = try container.decodeIfPresent(License.self, forKey: .license) ?? .none
