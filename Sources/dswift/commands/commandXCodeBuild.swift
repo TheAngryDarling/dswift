ec912473e19edbc24607a52ca8c658bbee7a00a3
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2019-10-26 19:21:26 -0400

    Removed isRunningFromXcode check Moved defer above the if let path...

--- Sources/dswift/commands/commandXCodeBuild.swift
+++ Sources/dswift/commands/commandXCodeBuild.swift
@@ -10,18 +10,13 @@ import Foundation
 extension Commands {
     /// Command for executing in Xcode Script Build Phase
     static func commandXcodeBuild(_ args: [String]) throws -> Int32 {
-        guard isRunningFromXcode else {
-            errPrint("This command can only be executed from Xcode")
-            return 1
-        }
-
         let currentWorkingDir = FileManager.default.currentDirectoryPath
-        if let path = ( ProcessInfo.processInfo.environment["PROJECT_DIR"] ?? ProcessInfo.processInfo.environment["SRCROOT"] ?? ProcessInfo.processInfo.environment["SOURCE_ROOT"]) {
-            FileManager.default.changeCurrentDirectoryPath(path)
-        }
         
         defer {
             FileManager.default.changeCurrentDirectoryPath(currentWorkingDir)
+        }
+        if let path = ( ProcessInfo.processInfo.environment["PROJECT_DIR"] ?? ProcessInfo.processInfo.environment["SRCROOT"] ?? ProcessInfo.processInfo.environment["SOURCE_ROOT"]) {
+            FileManager.default.changeCurrentDirectoryPath(path)
         }
        return try commandXcodeDSwiftBuild(args)
     }
