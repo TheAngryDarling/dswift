2d0a9de32e8e0da238e9d9a471434dcca3f97355
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2019-10-26 19:09:50 -0400

    Added new defaultLicenseReadMe methods for use with any licenses that do not have specific short license strings

--- Sources/dswift/licenses/License.swift
+++ Sources/dswift/licenses/License.swift
@@ -9,4 +9,17 @@ import Foundation
 
 public struct Licenses {
     private init() { }
+    
+    internal static func defaultLicenseReadMe(license: DSwiftSettings.License) -> String {
+        return """
+Copyright \(Calendar.current.component(.year, from: Date())) \(settings.authorName ?? "")
+This project is licensed under \(license.displayName) - see the [LICENSE.md](LICENSE.md) file for details
+"""
+    }
+    internal static func defaultLicenseReadMe() -> String {
+        return """
+Copyright \(Calendar.current.component(.year, from: Date())) \(settings.authorName ?? "")
+This project is licensed under the conditions provided in the [LICENSE.md](LICENSE.md) file
+"""
+    }
 }
