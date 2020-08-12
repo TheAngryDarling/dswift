2bd0bd22f6130b1b0fe6c747fe662af7ed90f8e0
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2020-08-12 12:45:06 -0400

    Added processing of dependencies for providers.  That way not only providers of the current working project can be dealt with

--- Sources/dswift/objects/PackageDescription.swift
+++ Sources/dswift/objects/PackageDescription.swift
@@ -65,10 +65,13 @@ public struct PackageDescription {
         case missingSwift(String)
         case missingPackageFolder(String)
         case missingPackageFile(String)
-        case unableToLoadDescription(String)
+        case unableToLoadDescription(String, String?)
+        case unableToLoadDependencies(String, String?)
         case unableToTransformDescriptionIntoObjects(Swift.Error, Data, String?)
         case unableToTransformPackageDumpIntoObjects(Swift.Error, Data, String?)
+        case unableToTransformDependenciesIntoObjects(Swift.Error, Data, String?)
         case minimumSwiftNotMet(current: String, required: String)
+        case dependencyMissingLocalPath(name: String, url: String, version: String)
     }
     
     private struct Description: Codable {
@@ -125,27 +128,111 @@ public struct PackageDescription {
         let type: String
     }
     
+    fileprivate struct _Dependency: Codable {
+        let name: String
+        let url: String
+        let version: String
+        let path: String
+        let dependencies: [_Dependency]
+    }
+    
+    public struct Dependency {
+        private let p: Any?
+        fileprivate var parent: Dependency? { return self.p as? Dependency }
+        let name: String
+        let url: String
+        let version: String
+        let path: String
+        let description: PackageDescription?
+        public private(set) var dependencies: [Dependency]
+        
+        fileprivate init(_ dependency: _Dependency,
+                         swiftPath: String,
+                         havingParent parent: Dependency? = nil) throws {
+            guard !dependency.path.isEmpty else {
+                throw Error.dependencyMissingLocalPath(name: dependency.name,
+                                                       url: dependency.url,
+                                                       version: dependency.version)
+            }
+            self.p = parent
+            self.name = dependency.name
+            self.url = dependency.url
+            self.version = dependency.version
+            self.path = dependency.path
+            self.description = try PackageDescription(swiftPath: swiftPath,
+                                                      packagePath: self.path,
+                                                      loadDependencies: false)
+            
+            self.dependencies = []
+            
+            for dep in dependency.dependencies {
+                // Stop recursive dependancies
+                if !self.hasExistingDependency(dep) {
+                    self.dependencies.append(try Dependency(dep, swiftPath: swiftPath, havingParent: self))
+                }
+            }
+        }
+        
+        private func contains(_ dep: _Dependency) -> Bool {
+            for subDep in self.dependencies {
+                if (subDep.name == dep.name && subDep.url == dep.url && subDep.version == dep.version)  {
+                    return true
+                } else if subDep.contains(dep) {
+                    return true
+                }
+            }
+            return false
+        }
+        
+        private func hasExistingDependency(_ dep: _Dependency) -> Bool {
+            guard let parent = self.parent else { return false }
+            guard !(parent.name == dep.name && parent.url == dep.url && parent.version == dep.version) else {
+                return true
+            }
+            guard !parent.contains(dep) else { return true }
+            
+            return parent.hasExistingDependency(dep)
+            
+        }
+    }
+    
     let name: String
     let path: String
     let pkgConfig: String?
     let providers: Dictionary<String, Array<String>>
     let targets: [Target]
+    let dependencies: [Dependency]
     /// Gets all providers/packages in this project
     var uniqueProviders: Dictionary<String, Array<String>> {
+        func combind(_ src: Array<String>, _ dest: Array<String>) -> Array<String> {
+            var rtn = src
+            for val in dest {
+                if !rtn.contains(val) {
+                    rtn.append(val)
+                }
+            }
+            return rtn.sorted()
+        }
         var rtn = self.providers
         
-        for target in targets {
+        for target in self.targets {
             for (manager, packages) in target.providers {
-                guard var ary = rtn[manager] else {
+                guard let ary = rtn[manager] else {
                     rtn[manager] = packages.sorted()
                     continue
                 }
-                for package in packages {
-                    if !ary.contains(package) {
-                        ary.append(package)
-                    }
+                rtn[manager] = combind(ary, packages)
+            }
+        }
+        
+        for dependancy in self.dependencies {
+            guard let description = dependancy.description else { continue }
+            for (manager, packages) in description.uniqueProviders {
+                guard let ary = rtn[manager] else {
+                    rtn[manager] = packages.sorted()
+                    continue
                 }
-                rtn[manager] = ary.sorted()
+                rtn[manager] = combind(ary, packages)
             }
         }
         
@@ -153,7 +240,8 @@ public struct PackageDescription {
     }
     
     public init(swiftPath: String = "/usr/bin/swift",
-                packagePath: String = FileManager.default.currentDirectoryPath) throws {
+                packagePath: String = FileManager.default.currentDirectoryPath,
+                loadDependencies: Bool) throws {
         if !FileManager.default.fileExists(atPath: swiftPath) { throw Error.missingSwift(swiftPath) }
         if !FileManager.default.fileExists(atPath: packagePath) { throw Error.missingPackageFolder(packagePath) }
         
@@ -179,20 +267,23 @@ public struct PackageDescription {
         try task.execute()
         task.waitUntilExit()
         
+        var data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let responseStr = String(data: data, encoding: .utf8)
+        
         if task.terminationStatus != 0 {
-            if let describeStr = String(data: pipe.fileHandleForReading.readDataToEndOfFile(), encoding: .utf8),
+            
+            if let describeStr = responseStr,
                 let m = try? describeStr.firstMatch(pattern: "requires a minimum Swift tools version of (?<minVer>(\\d+(\\.\\d+(\\.\\d+)?)?)) \\(currently (?<currentVer>\\d+(\\.\\d+(\\.\\d+)?)?)\\)"),
                 let match = m,
                 let minVer = match.value(withName: "minVer"),
                 let currentVer = match.value(withName: "currentVer") {
                 throw Error.minimumSwiftNotMet(current: currentVer, required: minVer)
             }
-            throw Error.unableToLoadDescription(packagePath)
+            throw Error.unableToLoadDescription(packagePath, responseStr)
         }
         
-        var data = pipe.fileHandleForReading.readDataToEndOfFile()
         // Convert output to utf8 string
-        if let describeStr = String(data: data, encoding: .utf8) {
+        if let describeStr = responseStr {
             
             // Find the start of the json string {
             if let r = describeStr.range(of: "{") {
@@ -224,14 +315,26 @@ public struct PackageDescription {
         #endif
         let dumpPipe = Pipe()
         dumpTask.standardOutput = dumpPipe
-        dumpTask.standardError = dumpPipe
+        dumpTask.standardError = FileHandle.nullDevice //dumpPipe
         
         try dumpTask.execute()
         dumpTask.waitUntilExit()
         
-        if dumpTask.terminationStatus != 0 { throw Error.unableToLoadDescription(packagePath) }
+        if dumpTask.terminationStatus != 0 {
+            throw Error.unableToLoadDescription(packagePath,
+                                                String(data: dumpPipe.fileHandleForReading.readDataToEndOfFile(),
+                                                       encoding: .utf8))
+        }
         
         data = dumpPipe.fileHandleForReading.readDataToEndOfFile()
+        if var dtaStr = String(data: data, encoding: .utf8) {
+            if dtaStr.hasPrefix("unable to restore state from") {
+                dtaStr = dtaStr.split(separator: "\n").dropFirst().map(String.init).joined(separator: "\n")
+                if let newDta = dtaStr.data(using: .utf8) {
+                    data = newDta
+                }
+            }
+        }
         let dump: PackageDump!
         do {
             dump = try decoder.decode(PackageDump.self, from: data)
@@ -263,6 +366,52 @@ public struct PackageDescription {
         
         self.targets = tgts
         
+        guard loadDependencies else {
+            self.dependencies = []
+            return
+        }
+        
+        let depTask = Process()
+        
+        depTask.executable = URL(fileURLWithPath: swiftPath)
+        depTask.currentDirectory = URL(fileURLWithPath: packagePath)
+        depTask.arguments = ["package", "show-dependencies", "--format", "json"]
+        
+        #if os(macOS)
+        // Send errors to null
+        depTask.standardInput = FileHandle.nullDevice
+        #endif
+        let depPipe = Pipe()
+        depTask.standardOutput = depPipe
+        depTask.standardError = depPipe
+        
+        try depTask.execute()
+        depTask.waitUntilExit()
+        
+        if depTask.terminationStatus != 0 {
+            throw Error.unableToLoadDependencies(packagePath,
+                                                 String(data: depPipe.fileHandleForReading.readDataToEndOfFile(),
+                                                       encoding: .utf8))
+        }
+        
+        data = depPipe.fileHandleForReading.readDataToEndOfFile()
+        if var dtaStr = String(data: data, encoding: .utf8) {
+            if dtaStr.hasPrefix("unable to restore state from") {
+                dtaStr = dtaStr.split(separator: "\n").dropFirst().map(String.init).joined(separator: "\n")
+                if let newDta = dtaStr.data(using: .utf8) {
+                    data = newDta
+                }
+            }
+        }
+        let dep: _Dependency!
+        do {
+            dep = try decoder.decode(_Dependency.self, from: data)
+        } catch {
+            throw Error.unableToTransformDependenciesIntoObjects(error, data, String(data: data, encoding: .utf8))
+        }
+        
+        let rootDep = try Dependency(dep, swiftPath: swiftPath)
+        self.dependencies = rootDep.dependencies
     }
 }
 
@@ -274,7 +423,14 @@ extension PackageDescription.Error: CustomStringConvertible {
             case .missingSwift(let path): return "Swift not found at \(path)"
             case .missingPackageFolder(let path): return "Package folder not found at \(path)"
             case .missingPackageFile(let path): return "Missing file at \(path)"
-            case .unableToLoadDescription(let path): return "Unable to load package description for \(path)"
+            case .unableToLoadDescription(let path, let response):
+                var rtn = "Unable to load package description for \(path)"
+                if let r = response { rtn += "\n" + r }
+                return rtn
+            case .unableToLoadDependencies(let path, let response):
+                var rtn = "Unable to load package dependencies for \(path)"
+                if let r = response { rtn += "\n" + r }
+                return rtn
             case .unableToTransformDescriptionIntoObjects(let err, _, let str):
                 var rtn: String = "Unable to parse package description\nError: \(err)"
                 if let s = str { rtn += "\nText: \(s)" }
@@ -284,9 +440,19 @@ extension PackageDescription.Error: CustomStringConvertible {
                 var rtn: String = "Unable to parse package dump\nError: \(err)"
                 if let s = str { rtn += "\nText: \(s)" }
                 return rtn
-            
+            case .unableToTransformDependenciesIntoObjects(let err, _, let str):
+                var rtn: String = "Unable to parse dependencies\nError: \(err)"
+                if let s = str { rtn += "\nText: \(s)" }
+                return rtn
             case .minimumSwiftNotMet(current: let current, required: let required):
                 return "Requires a minimum Swift tools version of \(required) (currently \(current))"
+            case .dependencyMissingLocalPath(name: let name, url: let url, version: let version):
+                 var rtn = "Dependency \(name) having url \(url)"
+                 if version != "unspecified" {
+                    rtn += " with version \(version)"
+                }
+                rtn += " is missing locally"
+                return rtn
         }
     }
 }
